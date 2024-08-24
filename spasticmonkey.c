b74b3a2e7a58d46b9474fa3e5281647148d5a897
Joel O'Callaghan <joelocallaghan@hotmail.co.nz>
2024-08-24 20:39:36 +1200

    key communication

--- spasticmonkey.c
+++ spasticmonkey.c
@@ -2,21 +2,35 @@
 #include <X11/X.h>
 #include <X11/Xlib.h>
 #include <time.h>
-
+#include <stdbool.h>
 // get info about display and make it useable variable (i think). 
 Display *dpy; 
+Window root;
+XEvent e;
+static int scr;
+
+static const char TERM[] = "st";
 
 
 int main() {
-  if (!(dpy = XOpenDisplay(NULL))){
-    fprintf(stdout, "failed to connect");
-  } else {
-    fprintf(stdout, "good");
-  }
+  if (!(dpy = XOpenDisplay(NULL)))
+    printf("spasticmonkey cannot open display");
+  else printf("good");
+  
+  scr = DefaultScreen(dpy);
+  root = RootWindow(dpy, scr);
 
+  XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("t")), Mod4Mask, root, True, GrabModeAsync, GrabModeAsync);
   
 
   for (;;){
-
+    XNextEvent(dpy, &e);
+    switch (e.type) {
+    case KeyPress:
+        //openterminal as place holder
+      system(TERM);
+      break;
   }
+  }
+  XCloseDisplay(dpy);
 }
--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
  
 Xephyr :5 -terminate -screen 1280x720 &
 sleep 3
-DISPLAY=:5 ./autostart.sh &
-DISPLAY=:5 ./spasticmonkey 
+DISPLAY=:5 ./spasticmonkey & 
+DISPLAY=:5 ./autostart.sh
